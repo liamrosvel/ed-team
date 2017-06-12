5d65fa8a9f8ea25b05410edff66aa30b865444ee
liamrosvel <liamrosvel@gmail.com>
2017-06-12 11:42:23 -0700

    SE AGREGO SCRIPT PARA MOSTRAR MENU
    Se agrego una clase por medio de Js para mostrar el menu offcanvas. Queda pendiente agregar un script para cambiar el icono al dar click.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 //Variables and objects
-
+nav        = document.getElementById('nav'),
+toggleMenu = document.getElementById('toggle-menu'),
+btnClose   = document.getElementById('button-close');
 
 //Functions
+var openMenu = function() {
+    nav.classList.toggle('active');
+}
 
-//Asign events
+//Asigning events
+
+toggleMenu.addEventListener('click', openMenu);
